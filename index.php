6490b0eba00565223ef8bb00adea8c67331c51f6
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-04-01 15:30:30 -0300

    caixa

--- index.php
+++ index.php
@@ -43,38 +43,61 @@ session_start();
         <h3>Caixa </h3>
 
         <div class="posisao">
-            <?php
+        <?php
+        // Include do arquivo PHP que contém a lógica do caixa
+        require_once 'caixa.php';
 
-            echo "Nome:";
-            echo $dados["nome"];
-            echo "<br>";
+        // Inicializa o objeto de caixa
+        $caixa = new Caixa(0);
 
-            echo "valor: R$";
-            echo $dados["valor"];
-            echo "<br>";
+        // Verifica se algum valor foi enviado via formulário
+        if(isset($_POST['valor'])){
+            $valor = $_POST['valor'];
 
+            // Verifica se o valor é válido
+            if(is_numeric($valor) && $valor > 0){
+                // Verifica se o botão de pagamento foi clicado
+                if(isset($_POST['pagar'])){
+                    // Verifica se o pagamento foi bem-sucedido
+                    if(Pagamento::pagarComPix($valor)){
+                        $mensagem = "Pagamento de " . $valor . " reais realizado com sucesso via PIX!";
+                    } else {
+                        $mensagem = "Falha ao realizar o pagamento via PIX!";
+                    }
+                } else {
+                    // Verifica se há saldo suficiente antes de realizar a compra
+                    if($caixa->removerDinheiro($valor)){
+                        $mensagem = "Compra de " . $valor . " reais realizada com sucesso!";
+                    } else {
+                        $mensagem = "Saldo insuficiente para realizar a compra!";
+                    }
+                }
+            } else {
+                $mensagem = "Por favor, insira um valor numérico válido.";
+            }
 
+            // Exibe a mensagem de retorno
+            echo "<p>" . $mensagem . "</p>";
+        }
+        ?>
 
+        <h2 class="mt-5">Saldo atual: <?php echo $caixa->getSaldo(); ?> reais</h2>
 
-            ?>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <div class="form-group">
+                <label for="valor">Valor:</label>
+                <input type="text" class="form-control" id="valor" name="valor" required>
+            </div>
+            <br>
+            <button type="submit" class="btn btn-primary" name="comprar">Retirar</button>
+            <button type="submit" class="btn btn-success" name="pagar">Pagar com PIX</button>
+        </form>
+    </div>
         </div>
     </div>
 
 
-    <div class="barra">
-        <h3>Função </h3>
-
-        
-        <a href="formcad.php" class="btn btn-dark btn-lg "> Depositar</a> 
-<br>
-<br>
-        <a href="formedit.php" class="btn btn-dark btn-lg "> Editar</a> 
-      <hr>
-
-      <a href="#" class="btn btn-dark btn-lg "> Sacar</a> 
-        
-
-    </div>
+    
 
 
     </main>
